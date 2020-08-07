15603fbeb2bd40d84917e64fcf0df974d54c0f07
John Lonetree <jlonetree@gmail.com>
2020-08-07 07:10:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -36,3 +36,13 @@ def return_string_array(array)
   array
 end
     
+def return_string_array(array)
+  counter = 0 
+  new_array = []
+  
+  while counter < array.length do
+    new_array.push(array[counter].to_s)
+    counter+= 1
+  end
+  new_array
+end

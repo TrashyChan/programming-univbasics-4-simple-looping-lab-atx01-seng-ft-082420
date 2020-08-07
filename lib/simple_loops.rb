e89e18af72fe0070cc89878d3b1d475f8c6580ad
John Lonetree <jlonetree@gmail.com>
2020-08-07 06:38:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,7 +2,7 @@ def loop_message_five_times(string)
   counter = 0 
   message = "Hello World."
   
-  while message[counter] do
+  while message[counter] < 5 do
     puts message[counter]
     counter += 1 
 end
